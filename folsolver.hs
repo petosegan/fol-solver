fdc05d6847b21a35ae61b2f3648c3f3a94ce5fd8
Richard Turner <rwturner@stanford.edu>
2015-03-25 11:11:18 -0700

    started substitution code
    also wrote some test cases

--- folsolver.hs
+++ folsolver.hs
@@ -39,3 +39,46 @@ renderSentence (ImplySentence s1 s2) = concat ["(",renderSentence s1, " => ", re
 renderSentence (IffSentence s1 s2) = concat ["(",renderSentence s1, " <=> ", renderSentence s2, ")"]
 renderSentence (QuantSentence qq vv ss) = "(" ++ show qq ++ vv ++ " {" ++ renderSentence ss ++ "})"
 
+defSentenceTfm :: (Sentence -> Sentence) -> Sentence -> Sentence
+defSentenceTfm tfm (NotSentence ss) = NotSentence (tfm ss)
+defSentenceTfm tfm (AndSentence s1 s2) = AndSentence (tfm s1) (tfm s2)
+defSentenceTfm tfm (OrSentence s1 s2) = OrSentence (tfm s1) (tfm s2)
+defSentenceTfm tfm (ImplySentence s1 s2) = ImplySentence (tfm s1) (tfm s2)
+defSentenceTfm tfm (IffSentence s1 s2) = IffSentence (tfm s1) (tfm s2)
+defSentenceTfm tfm (QuantSentence qq vv ss) = QuantSentence qq vv (tfm ss)
+
+type Substitution = (Variable, Term)
+
+type BindingList = [Substitution]
+
+substitute :: BindingList -> Sentence -> Sentence
+substitute bl (AtomicSentence aa) = AtomicSentence (substAtom bl aa)
+substitute bl (QuantSentence qq vv ss) = undefined
+substitute bl ss= defSentenceTfm (substitute bl) ss
+
+substAtom :: BindingList -> Atom -> Atom
+substAtom bl (Predication pp tt) = Predication pp (substTerm bl tt)
+substAtom bl (Equality t1 t2) = Equality (substTerm bl t1) (substTerm bl t2)
+substAtom bl (AtomBool bb) = AtomBool bb
+
+substTerm :: BindingList -> Term -> Term
+substTerm bl (FuncTerm ff tt) = FuncTerm ff (substTerm bl tt)
+substTerm bl (ConstTerm cc) = ConstTerm cc
+substTerm bl (VaryTerm vv) = substVary bl vv
+
+substVary :: BindingList -> Variable -> Term
+substVary = undefined
+-- test cases
+
+test_sentence = QuantSentence ForAll v1 s1
+
+v1 = "m"
+v1T = VaryTerm v1
+s1 = (ImplySentence isMonkeyS likesBananasS)
+isMonkeyS = AtomicSentence isMonkeyA
+isMonkeyA = Predication "IsMonkey" v1T
+
+likesBananasS = AtomicSentence likesBananasA
+likesBananasA = Predication "LikesBananas" v1T
+
+test_sentence2 = QuantSentence ThereExists v1 isMonkeyS
